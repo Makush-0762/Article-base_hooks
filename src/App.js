e56c19730d6862ec130021fc32350d043fe92e09
Kushka Roman <romanovich9102@gmail.com>
2023-11-08 22:11:42 +0200

    fix design

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './UserList.css';
 import axios from "axios";
 
 function App() {
+  
   const [isAddAssignVisible, setIsAddAssignVisible] = useState({});
   const [selectedUsers, setSelectedUsers] = useState([]);
-  const [group1Users, setGroup1Users] = useState([]);
   const [users, setUsers] = useState([]);
   const [usersGroup, setUsersGroup] = useState([]);
   const [dataSentToServer, setDataSentToServer] = useState(false);
@@ -52,7 +52,7 @@ function App() {
     fetchData();
   }, [dataSentToServer]);
 
-  console.log(usersGroup);
+  // console.log(usersGroup);
 
   const handleMouseEnter = (userId) => {
     setIsAddAssignVisible((prevState) => ({
@@ -68,10 +68,6 @@ function App() {
     }));
   };
 
-  const handleAddToGroup1 = () => {
-    setGroup1Users((prevGroup1Users) => [...prevGroup1Users, ...selectedUsers]);
-    setSelectedUsers([]);
-  };
 
   return (
     <div className="App">
@@ -149,103 +145,6 @@ function App() {
 export default App;
 
 
-// import React, { useState, useEffect } from 'react';
-// import './App.css';
-// import './UserList.css';
-// import user_1 from './img/user_1.png';
-// import user_2 from './img/user_2.png';
-// import user_3 from './img/user_3.jpg';
-
-// function App() {
-//   const users = [
-//     { id: 7, name: 'Кулик Олександр Вікторович', photo: user_1 },
-//     { id: 45, name: 'Високий Степан Олександрович', photo: user_2 },
-//     { id: 80, name: 'Ряба Наталя Олегівна', photo: user_3 },
-//   ];
-
-//   const [isAddAssignVisible, setIsAddAssignVisible] = useState({});
-//   const [selectedUsers, setSelectedUsers] = useState({});
-//   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-
-//   useEffect(() => {
-//     console.log('Обрані користувачі змінились:', selectedUsers);
-//   }, [selectedUsers]);
-
-//   const handleMouseEnter = (userId) => {
-//     setIsAddAssignVisible((prevState) => ({
-//       ...prevState,
-//       [userId]: true,
-//     }));
-//   };
-
-//   const handleMouseLeave = (userId) => {
-//     setIsAddAssignVisible((prevState) => ({
-//       ...prevState,
-//       [userId]: false,
-//     }));
-//   };
-
-//   const handleAddToSelectedUsers = (user) => {
-//     setSelectedUsers((prevUsers) => ({
-//       ...prevUsers,
-//       [user.id]: true,
-//     }));
-//     setIsButtonDisabled(true); // Відключаємо кнопку після натискання
-//   };
-
-//   return (
-//     <div className="App">
-//       <div className="user-list">
-//         <center>
-//           <h1>Список користувачів:</h1>
-//         </center>
-//         <ul>
-//           {users.map((user) => (
-//             <li
-//               key={user.id}
-//               className="user-item"
-//               onMouseEnter={() => handleMouseEnter(user.id)}
-//               onMouseLeave={() => handleMouseLeave(user.id)}
-//             >
-//               <div className="user_info">
-//                 <div className="body_user-img">
-//                   <img src={user.photo} alt={`user ${user.name}`} />
-//                 </div>
-//                 <span>{user.name}</span>
-//               </div>
-//               {isAddAssignVisible[user.id] && !selectedUsers[user.id] && (
-//                 <button
-//                   className="add-button"
-//                   onClick={() => handleAddToSelectedUsers(user)}
-//                   disabled={isButtonDisabled} // Включаємо або відключаємо кнопку
-//                 >
-//                   Додати до співрозмовників
-//                 </button>
-//               )}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//       <div className="selected-user">
-//         <h2>Обрані користувачі:</h2>
-//         <ul>
-//           {Object.keys(selectedUsers).map((userId) => {
-//             const selectedUser = users.find((user) => user.id === parseInt(userId));
-//             return (
-//               <li key={userId} title={selectedUser.name}>
-//                   <div className="body_user-img">
-//                     < img src={selectedUser.photo} alt={`user ${selectedUser.name}`} />
-//                   </div>
-//               </li>
-//             );
-//           })}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 
@@ -257,135 +156,3 @@ export default App;
 
 
 
-// import React, { useState} from 'react';
-// import './App.css';
-// import './UserList.css';
-// import user_1 from './img/user_1.png';
-// import user_2 from './img/user_2.png';
-// import user_3 from './img/user_3.jpg';
-
-// function App() {
-
-//   const users = [
-//     { id: 7, name: 'Кулик Олександр Вікторович', photo: user_1},
-//     { id: 45, name: 'Високий Степан Олександрович', photo: user_2},
-//     { id: 80, name: 'Ряба Наталя Олегівна', photo: user_3},
-// ];
-
-//     const [isAddAssignVisible, setIsAddAssignVisible] = useState({});
-    
-//     const handleMouseEnter = (userId) => {
-//         setIsAddAssignVisible((prevState) => ({
-//         ...prevState,
-//         [userId]: true,
-//         }));
-//     };
-    
-//     const handleMouseLeave = (userId) => {
-//         setIsAddAssignVisible((prevState) => ({
-//         ...prevState,
-//         [userId]: false,
-//         })); 
-//     };
-//   return (
-//     <div className="App">
-//         <div className="user-list">
-//             <center><h1>Список користувачів:</h1></center>
-//             <ul>
-//                 {users.map((user) => (
-//                     <li key={user.id} className="user-item" onMouseEnter={() => handleMouseEnter(user.id)} 
-//                                                             onMouseLeave={() => handleMouseLeave(user.id)}>
-//                     <div className="user_info">
-//                       <div className="body_user-img">
-//                         <img src={user.photo} alt={`user ${user.name}`} />
-//                       </div>
-//                       <span>{user.name}</span> 
-//                     </div>
-//                     {isAddAssignVisible[user.id] && (
-//                         <button className="add-button" 
-//                         onClick={() => console.log(`Користувач: із id-${user.id} ${user.name} доданий до співрозмовників`)}>
-//                             Додати до співрозмовників
-//                         </button>
-//                     )}
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const codeCat = [100, 101, 102, 103, 200, 201, 202, 203, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404];
-// const [hoveredCat, setHoveredCat] = useState(null);
-
-// const [todos, setTodos] = useState([]);
-// const [newTodo, setNewTodo] = useState('');
-
-// // const handleInputChange = (e) => {
-// //   setNewTodo(e.target.value);
-// // };
-
-// const handleAddTodo = () => {
-//   if (newTodo.trim() !== '') {
-//     setTodos([...todos, newTodo]);
-//     setNewTodo('');
-//   }
-// };
-
-
-
-      // {/* <div>
-      //   <input type="text" value={newTodo} onChange={handleInputChange} placeholder="Нове завдання" />
-      //   <button onClick={handleAddTodo}>Додати</button>
-      //   <ul>
-      //     {todos.map((todo, index) => (
-      //       <li key={index}>{todo}</li>
-      //     ))}
-      //   </ul>
-      // </div> */}
-      //       {/* <header className="App-header">
-      //   {codeCat.map((cat) => (
-      //     <div
-      //       key={cat}
-      //       className="photo-container"
-      //       onMouseEnter={() => setHoveredCat(cat)}
-      //       onMouseLeave={() => setHoveredCat(null)}
-      //     >
-      //       <img src={`https://http.cat/${cat}.jpg`} className="App-logo" alt={`cat-${cat}`} />
-      //       {hoveredCat === cat && <img src="icon.png" className="icon" alt="icon" />}
-      //     </div>
-      //   ))}
-      //   <p>
-      //     Edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      //   <a
-      //     className="App-link"
-      //     href="https://reactjs.org"
-      //     target="_blank"
-      //     rel="noopener noreferrer"
-      //   >
-      //     Learn React
-      //   </a>
-      // </header> */}
---
+++ src/FirstUseEffect.js
@@ -0,0 +1,60 @@
+import React, { useState} from 'react';
+import './App.css';
+import './UserList.css';
+import user_1 from './img/user_1.png';
+import user_2 from './img/user_2.png';
+import user_3 from './img/user_3.jpg';
+
+function FirstUseEffect() {
+
+const users = [
+    { id: 7, name: 'Кулик Олександр Вікторович', photo: user_1},
+    { id: 45, name: 'Високий Степан Олександрович', photo: user_2},
+    { id: 80, name: 'Ряба Наталя Олегівна', photo: user_3},
+];
+
+const [isAddAssignVisible, setIsAddAssignVisible] = useState({});
+    
+const handleMouseEnter = (userId) => {
+    setIsAddAssignVisible((prevState) => ({
+    ...prevState,
+    [userId]: true,
+    }));
+};
+
+const handleMouseLeave = (userId) => {
+    setIsAddAssignVisible((prevState) => ({
+    ...prevState,
+    [userId]: false,
+    })); 
+};
+return (
+    <div className="App">
+        <div className="user-list">
+            <center><h1>Список користувачів:</h1></center>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id} className="user-item" onMouseEnter={() => handleMouseEnter(user.id)} 
+                                                            onMouseLeave={() => handleMouseLeave(user.id)}>
+                    <div className="user_info">
+                        <div className="body_user-img">
+                            <img src={user.photo} alt={`user ${user.name}`} />
+                        </div>
+                        <span>{user.name}</span> 
+                    </div>
+                    {isAddAssignVisible[user.id] && (
+                        <button className="add-button" 
+                        onClick={() => console.log(`Користувач: із id-${user.id} ${user.name} доданий до співрозмовників`)}>
+                            Додати до співрозмовників
+                        </button>
+                    )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    
+    </div>
+    );
+}
+
+export default FirstUseEffect;
